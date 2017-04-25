6f72ac70797ddbf15f3ce4806b0a60641b5132f4
zariweya <adrian.sanchez.rijo@gmail.com>
2017-04-25 12:56:44 +0100

    quitando html's del blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -52,15 +52,15 @@
 								<div class="beans-slideset">
 									<!-- beans-slide -->
 									<div class="beans-slide">
-										<a href="blog-single-slider.html"><img src="http://placehold.it/870x400" alt="image description" class="img-responsive"></a>
+										<a href="#"><img src="https://placehold.it/870x400" alt="image description" class="img-responsive"></a>
 									</div>
 									<!-- beans-slide -->
 									<div class="beans-slide">
-										<a href="blog-single-slider.html"><img src="http://placehold.it/870x400" alt="image description" class="img-responsive"></a>
+										<a href="#"><img src="https://placehold.it/870x400" alt="image description" class="img-responsive"></a>
 									</div>
 									<!-- beans-slide -->
 									<div class="beans-slide">
-										<a href="blog-single-slider.html"><img src="http://placehold.it/870x400" alt="image description" class="img-responsive"></a>
+										<a href="#"><img src="https://placehold.it/870x400" alt="image description" class="img-responsive"></a>
 									</div>
 								</div>
 							</div>
@@ -70,27 +70,27 @@
 						</div>
 					</div>
 					<div class="blog-txt">
-						<h2><a href="blog-single-slider.html">Why Does Music Feel So Good?</a></h2>
+						<h2><a href="#">Why Does Music Feel So Good?</a></h2>
 						<ul class="meta list-inline">
 							<li>
 								<i class="fa fa-user"></i>
-								By <a href="blog-single-slider.html">Admin name</a>
+								By <a href="#">Admin name</a>
 							</li>
 							<li>
 								<i class="fa fa-tags"></i>
-								<a href="blog-single-slider.html">Music</a>, <a href="blog-single-slider.html">Technologies</a>, <a href="blog-single-slider.html">People</a> 
+								<a href="#">Music</a>, <a href="#">Technologies</a>, <a href="#">People</a> 
 							</li>
 							<li>
 								<i class="fa fa-heart"></i>
-								<a href="blog-single-slider.html">14</a>
+								<a href="#">14</a>
 							</li>
 							<li>
 								<i class="fa fa-comments"></i>
-								<a href="blog-single-slider.html">320</a>
+								<a href="#">320</a>
 							</li>
 						</ul>
 						<p>In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied  newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments.</p>
-						<a href="blog-single-slider.html" class="more">[ CONTINUE READING ]</a>
+						<a href="#" class="more">[ CONTINUE READING ]</a>
 						<div class="box-holder">
 							<span class="icon"><i class="fa fa-picture-o"></i></span>
 							<time datetime="2015-02-18">
@@ -102,30 +102,30 @@
 				<!-- blog-post-v1 -->
 				<article class="blog-post-v1">
 					<div class="img-box">
-						<a href="blog-single-image.html"><img src="http://placehold.it/870x400" alt="image description" class="img-responsive"></a>
+						<a href="#"><img src="https://placehold.it/870x400" alt="image description" class="img-responsive"></a>
 					</div>
 					<div class="blog-txt">
-						<h2><a href="blog-single-image.html">Intelegent transitions in ux design</a></h2>
+						<h2><a href="#">Intelegent transitions in ux design</a></h2>
 						<ul class="meta list-inline">
 							<li>
 								<i class="fa fa-user"></i>
-								By <a href="blog-single-image.html">Admin name</a>
+								By <a href="#">Admin name</a>
 							</li>
 							<li>
 								<i class="fa fa-tags"></i>
-								<a href="blog-single-image.html">Music</a>, <a href="blog-single-image.html">Technologies</a>, <a href="blog-single-image.html">People</a> 
+								<a href="#">Music</a>, <a href="#">Technologies</a>, <a href="#">People</a> 
 							</li>
 							<li>
 								<i class="fa fa-heart"></i>
-								<a href="blog-single-image.html">14</a>
+								<a href="#">14</a>
 							</li>
 							<li>
 								<i class="fa fa-comments"></i>
-								<a href="blog-single-image.html">320</a>
+								<a href="#">320</a>
 							</li>
 						</ul>
 						<p>In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied  newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments.</p>
-						<a href="blog-single-image.html" class="more">[ CONTINUE READING ]</a>
+						<a href="#" class="more">[ CONTINUE READING ]</a>
 						<div class="box-holder">
 							<span class="icon"><i class="fa fa-picture-o"></i></span>
 							<time datetime="2015-02-18">
@@ -138,37 +138,37 @@
 				<article class="blog-post-v1">
 					<div class="img-box">
 						<div class="box">
-							<a href="blog-single-3column.html"><img src="http://placehold.it/275x275" alt="image description" class="img-responsive"></a>
+							<a href="#"><img src="https://placehold.it/275x275" alt="image description" class="img-responsive"></a>
 						</div>
 						<div class="box">
-							<a href="blog-single-3column.html"><img src="http://placehold.it/275x275" alt="image description" class="img-responsive"></a>
+							<a href="#"><img src="https://placehold.it/275x275" alt="image description" class="img-responsive"></a>
 						</div>
 						<div class="box">
-							<a href="blog-single-3column.html"><img src="http://placehold.it/275x275" alt="image description" class="img-responsive"></a>
+							<a href="#"><img src="https://placehold.it/275x275" alt="image description" class="img-responsive"></a>
 						</div>
 					</div>
 					<div class="blog-txt">
-						<h2><a href="blog-single-3column.html">This is multi images post</a></h2>
+						<h2><a href="#">This is multi images post</a></h2>
 						<ul class="meta list-inline">
 							<li>
 								<i class="fa fa-user"></i>
-								By <a href="blog-single-3column.html">Admin name</a>
+								By <a href="#">Admin name</a>
 							</li>
 							<li>
 								<i class="fa fa-tags"></i>
-								<a href="blog-single-3column.html">Music</a>, <a href="blog-single-3column.html">Technologies</a>, <a href="blog-single-3column.html">People</a> 
+								<a href="#">Music</a>, <a href="#">Technologies</a>, <a href="#">People</a> 
 							</li>
 							<li>
 								<i class="fa fa-heart"></i>
-								<a href="blog-single-3column.html">14</a>
+								<a href="#">14</a>
 							</li>
 							<li>
 								<i class="fa fa-comments"></i>
-								<a href="blog-single-3column.html">320</a>
+								<a href="#">320</a>
 							</li>
 						</ul>
 						<p>In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied  newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments.</p>
-						<a href="blog-single-3column.html" class="more">[ CONTINUE READING ]</a>
+						<a href="#" class="more">[ CONTINUE READING ]</a>
 						<div class="box-holder">
 							<span class="icon"><i class="fa fa-picture-o"></i></span>
 							<time datetime="2015-02-18">
@@ -183,27 +183,27 @@
 						<iframe src="https://player.vimeo.com/video/78907778" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
 					<div class="blog-txt">
-						<h2><a href="blog-single-video-vimeo.html">Video post</a></h2>
+						<h2><a href="#">Video post</a></h2>
 						<ul class="meta list-inline">
 							<li>
 								<i class="fa fa-user"></i>
-								By <a href="blog-single-video-vimeo.html">Admin name</a>
+								By <a href="#">Admin name</a>
 							</li>
 							<li>
 								<i class="fa fa-tags"></i>
-								<a href="blog-single-video-vimeo.html">Music</a>, <a href="blog-single-video-vimeo.html">Technologies</a>, <a href="blog-single-video-vimeo.html">People</a> 
+								<a href="#">Music</a>, <a href="#">Technologies</a>, <a href="#">People</a> 
 							</li>
 							<li>
 								<i class="fa fa-heart"></i>
-								<a href="blog-single-video-vimeo.html">14</a>
+								<a href="#">14</a>
 							</li>
 							<li>
 								<i class="fa fa-comments"></i>
-								<a href="blog-single-video-vimeo.html">320</a>
+								<a href="#">320</a>
 							</li>
 						</ul>
 						<p>In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied  newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments.</p>
-						<a href="blog-single-video-vimeo.html" class="more">[ CONTINUE READING ]</a>
+						<a href="#" class="more">[ CONTINUE READING ]</a>
 						<div class="box-holder">
 							<span class="icon"><i class="fa fa-video-camera"></i></span>
 							<time datetime="2015-02-18">
@@ -218,27 +218,27 @@
 						<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/207659137&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
 					</div>
 					<div class="blog-txt">
-						<h2><a href="blog-single-soundcloud.html">soundcloud post</a></h2>
+						<h2><a href="#">soundcloud post</a></h2>
 						<ul class="meta list-inline">
 							<li>
 								<i class="fa fa-user"></i>
-								By <a href="blog-single-soundcloud.html">Admin name</a>
+								By <a href="#">Admin name</a>
 							</li>
 							<li>
 								<i class="fa fa-tags"></i>
-								<a href="blog-single-soundcloud.html">Music</a>, <a href="blog-single-soundcloud.html">Technologies</a>, <a href="blog-single-soundcloud.html">People</a> 
+								<a href="#">Music</a>, <a href="#">Technologies</a>, <a href="#">People</a> 
 							</li>
 							<li>
 								<i class="fa fa-heart"></i>
-								<a href="blog-single-soundcloud.html">14</a>
+								<a href="#">14</a>
 							</li>
 							<li>
 								<i class="fa fa-comments"></i>
-								<a href="blog-single-soundcloud.html">320</a>
+								<a href="#">320</a>
 							</li>
 						</ul>
 						<p>In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied  newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments.</p>
-						<a href="blog-single-soundcloud.html" class="more">[ CONTINUE READING ]</a>
+						<a href="#" class="more">[ CONTINUE READING ]</a>
 						<div class="box-holder">
 							<span class="icon"><i class="fa fa-volume-down"></i></span>
 							<time datetime="2015-02-18">
@@ -250,27 +250,27 @@
 				<!-- blog-post-v1 -->
 				<article class="blog-post-v1">
 					<div class="blog-txt">
-						<h2><a href="blog-single-image.html">Intelegent transitions in ux design</a></h2>
+						<h2><a href="#">Intelegent transitions in ux design</a></h2>
 						<ul class="meta list-inline">
 							<li>
 								<i class="fa fa-user"></i>
-								By <a href="blog-single-image.html">Admin name</a>
+								By <a href="#">Admin name</a>
 							</li>
 							<li>
 								<i class="fa fa-tags"></i>
-								<a href="blog-single-image.html">Music</a>, <a href="blog-single-image.html">Technologies</a>, <a href="blog-single-image.html">People</a> 
+								<a href="#">Music</a>, <a href="#">Technologies</a>, <a href="#">People</a> 
 							</li>
 							<li>
 								<i class="fa fa-heart"></i>
-								<a href="blog-single-image.html">14</a>
+								<a href="#">14</a>
 							</li>
 							<li>
 								<i class="fa fa-comments"></i>
-								<a href="blog-single-image.html">320</a>
+								<a href="#">320</a>
 							</li>
 						</ul>
 						<p>In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied  newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments.</p>
-						<a href="blog-single-image.html" class="more">[ CONTINUE READING ]</a>
+						<a href="#" class="more">[ CONTINUE READING ]</a>
 						<div class="box-holder">
 							<span class="icon"><i class="fa fa-pencil"></i></span>
 							<time datetime="2015-02-18">
@@ -282,14 +282,14 @@
 				<!-- blog-footer -->
 				<nav class="blog-footer">
 					<div class="btn-box">
-						<a href="blog-single-image.html" class="btn">PREVIOUS</a>
-						<a href="blog-single-image.html" class="btn">NEXT</a>
+						<a href="#" class="btn">PREVIOUS</a>
+						<a href="#" class="btn">NEXT</a>
 					</div>
 					<ul class="b-pagination list-unstyled">
-						<li><a href="blog-single-image.html">1</a></li>
-						<li class="active"><a href="blog-single-image.html">2</a></li>
-						<li><a href="blog-single-image.html">3</a></li>
-						<li><a href="blog-single-image.html">4</a></li>
+						<li><a href="#">1</a></li>
+						<li class="active"><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
 					</ul>
 				</nav>
 			</div>
@@ -318,7 +318,7 @@
 						<div id="tab1">
 							<article class="box">
 								<div class="img-box">
-									<img src="http://placehold.it/64x64" alt="image description">
+									<img src="https://placehold.it/64x64" alt="image description">
 								</div>
 								<div class="holder">
 									<time datetime="2015-01-01">13 MAY 2015</time>
@@ -327,7 +327,7 @@
 							</article>
 							<article class="box">
 								<div class="img-box">
-									<img src="http://placehold.it/64x64" alt="image description">
+									<img src="https://placehold.it/64x64" alt="image description">
 								</div>
 								<div class="holder">
 									<time datetime="2015-01-01">13 MAY 2015</time>
@@ -336,7 +336,7 @@
 							</article>
 							<article class="box">
 								<div class="img-box">
-									<img src="http://placehold.it/64x64" alt="image description">
+									<img src="https://placehold.it/64x64" alt="image description">
 								</div>
 								<div class="holder">
 									<time datetime="2015-01-01">13 MAY 2015</time>
